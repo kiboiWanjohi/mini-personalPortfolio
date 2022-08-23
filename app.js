1b78b5ee034acdd7b6303daea091e9dc31e9b7f2
kiboiWanjohi <sammy313.sw@gmail.com>
2022-08-23 12:23:30 +0300

    Projects Page

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const navmenu = document.querySelector(".nav-menu");
 const hambuger = document.querySelector(".hambuger");
+
 hambuger.addEventListener("click", dropMenu);
 function dropMenu() {
   hambuger.classList.toggle("change");
   navmenu.classList.toggle("active");
 }
-// function myFunction(x) {
-//   x.classList.toggle("change");
-// }
